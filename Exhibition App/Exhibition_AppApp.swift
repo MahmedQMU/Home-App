e0fa1014232d1766bcae1b30cbef823a59f76704
Mustafa Ahmed <Mahmed@qmu.ac.uk>
2024-04-27 06:38:15 +0100

    Initial Commit

---
+++ Exhibition App/Exhibition_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Exhibition_AppApp.swift
+//  Exhibition App
+//
+//  Created by Mustafa Ahmed on 27/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Exhibition_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
